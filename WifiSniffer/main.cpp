79e47fa46ced049a61b9b995a5db6708b5780532
Alix Taylor <artaylor2@students.nic.edu>
2021-04-20 21:56:11 -0700

    Created driver for testing.

---
+++ WifiSniffer/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Taylor, Alixandra (artaylor2@students.nic.edu)
+ * @brief Test driver for wifi sniffer
+ * @version 0.1
+ * @date 2021-04-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "wifiSniffer.h"
+
+int main()
+{
+    sniffWifi();
+
+    return 0;
+}
